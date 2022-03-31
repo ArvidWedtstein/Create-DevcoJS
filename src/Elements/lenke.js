89f9acdc6401bd7eae4cf8b71920e9cb0b73df3b
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-03-31 14:23:56 +0200

    c

--- src/Elements/lenke.js
+++
@@ -1,8 +0,0 @@
-class Lenke extends HTMLElement {
-    constructor() {
-      // Always call super first in constructor
-      super();
-    }
-  }
-  customElements.define('n-lenke', Lenke, {extends: 'a'});
-  
